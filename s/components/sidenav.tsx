fac19c9fdee2120246ed8f487a91cd40ec081cf9
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-06-02 01:49:48 +0200

    add: sidenav component

--- s/components/sidenav.tsx
+++ s/components/sidenav.tsx
@@ -1,8 +1,9 @@
 import {styles} from "../styles/sidenav-styles"
 import { useContext, useState } from "react";
 import PageStore from "../states-store/states/page-store";
-import { Image, Pressable, View } from "react-native";
+import { Image, Pressable, View, Text } from "react-native";
 import BlurView from "expo-blur/build/BlurView";
+
 
 const Sidenav = () => {
 	const pageStore = useContext(PageStore);
@@ -10,10 +11,11 @@ const Sidenav = () => {
       return(
 				<BlurView style={styles.container}>
 					<View style={styles.elContainer}>
-						<Image style={styles.iconMain} source={require("../images/csgo-icon.png")} />
+						<Image style={styles.iconMain} source={require("../images/csgo-icon3.png")} />
+						<Text style={styles.text}>Clicker</Text>
 					</View>
 					<Pressable onPress={() => {pageStore.setInventoryVisible(bool), setBool(!bool)}}>
-					<Image style={styles.icon} source={require("../images/case-icon.png")} />
+						<Image style={styles.icon} source={require("../images/briefcase.jpg")} />
 					</Pressable>
 				</BlurView>
       );
