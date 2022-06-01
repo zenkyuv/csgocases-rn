85a27324e5ef0ccc62e3fc9fdb3ebbb4dfc5c87b
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-06-02 01:51:00 +0200

    add: profilebar component

---
+++ s/components/profilebar.tsx
@@ -0,0 +1,15 @@
+import { View, Text, Image } from "react-native"
+import BlurView from "expo-blur/build/BlurView";
+import { styles } from "../styles/profilebar-styles"
+
+const ProfileBar = () => {
+	return (
+		<BlurView intensity={80} style={styles.container}>
+			<Image style={styles.image} source={require("../images/profile-img.jpg")} />
+			<Image style={styles.rank} source={require("../images/silver4.png")} />
+			<View style={styles.friends}><Text style={styles.info}>i</Text></View>
+		</BlurView>
+	)
+}
+
+export default ProfileBar
