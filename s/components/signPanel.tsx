2a7fe9b6c22eb9384c190b2b7a4c5a475318cee2
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-06-12 17:59:44 +0200

    add: signPanel

---
+++ s/components/signPanel.tsx
@@ -0,0 +1,59 @@
+import { Formik } from "formik";
+import { useContext, useState } from "react";
+import { Button, Text, TextInput, View } from "react-native";
+import { createUser, signUser } from "../database/auth";
+import UserStore from "../states-store/states/userStore";
+import { styles } from "../styles/signPanelStyles";
+
+
+
+export default function SignPanel(props: any) {
+	const userStore = useContext(UserStore);
+	const [showPanel, setPanel] = useState({
+		signIn: true,
+		signUp: false
+	})
+	
+	const handleSignUp = (userData:any) => {
+		createUser({ userData, userStore })
+	}
+
+console.log(showPanel.signIn)
+	const handleSignIn = (userData: any) => {
+		signUser({ userData, userStore });
+	}
+	return (<View style={styles.container}>
+		<Formik initialValues={{ email: "", password: "" }}
+			onSubmit={values => showPanel.signIn ? handleSignIn(values) : handleSignUp(values)}
+		>
+			{({ handleChange, handleBlur, handleSubmit, values }) => (
+				<>
+					<Text>Email</Text>
+					<TextInput
+						onChangeText={handleChange('email')}
+						style={styles.textInput}
+						value={values.email}
+					/>
+					<Text>Password</Text>
+					<TextInput
+						secureTextEntry={true}
+						textContentType={"password"}
+						onChangeText={handleChange('password')}
+						style={styles.textInput}
+						value={values.password}
+					/>
+					<Text style={styles.smallText}>{showPanel.signIn ? "Not registered yet?" : "Back to "}
+						<Text onPress={() => setPanel({
+							signIn: !showPanel.signIn,
+							signUp: !showPanel.signUp
+						})}
+							style={styles.registerText}>{showPanel.signIn ? " Register" : "Login"}</Text></Text>
+					<Button
+						onPress={() => handleSubmit()}
+						title={showPanel.signIn ? "Login" : "Register"}
+						color="#841584"
+						accessibilityLabel="Learn more about this purple button" /></>)}
+			</Formik>
+</View>)
+	};
+	
