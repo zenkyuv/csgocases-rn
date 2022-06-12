712d53e1f368bf15f0f78722d9cfb9f961282488
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-06-12 17:54:53 +0200

    style: signPanel

---
+++ s/styles/signPanelStyles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+	container: {
+		padding: 10,
+		height: "100%",
+		width: "50%",
+		alignItems: "center",
+		justifyContent: "center",
+		alignSelf: "center"
+	},
+	textInput: {
+		height: 20,
+		width: "100%",
+		borderColor: "black",
+		borderWidth: 1
+	},
+	smallText: {
+		fontSize: 10,
+		alignSelf: "flex-end"
+	},
+	registerText: {
+		color: "orange"
+	}
+})
